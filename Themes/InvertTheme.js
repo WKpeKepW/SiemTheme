d508b1c6296e1c5a6aebd19ae67afbf9f40bab9e
WKpeKepW <dan9o5ru@gmail.com>
2023-09-17 19:11:06 +0400

    Loops not an exit

--- Themes/InvertTheme.js
+++ Themes/InvertTheme.js
@@ -1,16 +1,12 @@
 function InvertTheme(){
     Change('body','filter','invert(1)')
     //Change('.pt-icons','filter','invert(1)',true,'#legacyApplicationFrame')
-
-    //Change('.mc-navbar','filter','invert(1)',true,null,10000)
-
-    //Change('.mc-navbar','background-color','#31200d',true,null,10000)
-
-    //Change('mc-navbar-item mc-navbar-focusable-item mc-dropdown-trigger mc-horizontal ng-star-inserted mc-active mc-pressed','background-color','black',true)
-    //Change('mc-navbar-item mc-navbar-focusable-item mc-dropdown-trigger mc-horizontal ng-star-inserted','background-color','#31200d',true)
-    //Change('mc-navbar-item, mc-navbar-focusable-item, mc-dropdown-trigger, mc-horizontal, ng-star-inserted','background-color','black',true) // изменить условие нахождения в core 
-    // checkReturn(()=>{
-    //     console.log($('mc-navbar-item, mc-navbar-focusable-item, mc-dropdown-trigger, mc-horizontal, ng-star-inserted').css('background-color'))// проверить найдет ли css с таким длинным названием
-    // })
-    //if($('.mc-active'))
+    //Change('.pt-icons','filter','invert(1) !important',true,'#legacyApplicationFrame')
+    Change('.statistics-footer','background-color','#83ffd152',true,'#legacyApplicationFrame')
+    Change('.statistics-footer','border-top','#83ffd152',true,'#legacyApplicationFrame')
+    Change('.mc-navbar','filter','invert(1)',true,null,10000)
+    Change('.mc-navbar','background-color','#31200d',true,null,10000)
+    Change('mc-navbar-item, mc-navbar-focusable-item, mc-dropdown-trigger, mc-horizontal, ng-star-inserted','background-color','#31200d',true)
+    Change('.mc-navbar-item.mc-active, .mc-navbar-brand.mc-active, .mc-navbar-toggle.mc-active','background','black',true)
+    Change('.events-pdql_changed','background-color','#1b83a814',true,'#legacyApplicationFrame')
 }
